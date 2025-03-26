5acff731d3cff78b8c5753242cd558d3c0062a90
maryyyz <mashaskibams@gmail.com>
2025-03-26 17:53:52 +0300

    thr1.1

--- SoftwareDirectory/SoftwareDirectory/SoftwareDirectory.cpp
+++ SoftwareDirectory/SoftwareDirectory/SoftwareDirectory.cpp
@@ -6,7 +6,6 @@
 
 using namespace std;
 
-// Класс Система аутентификации
 class AuthenticationSystem {
 public:
     bool login(string username, string password) {
@@ -18,7 +17,6 @@ public:
     }
 };
 
-// Класс Пользователь
 class User {
 public:
     string name;
@@ -28,20 +26,46 @@ public:
     void viewSoftware() { cout << "Просмотр ПО пользователя: " << name << endl; }
 };
 
-// Абстрактный класс Программное обеспечение
+class SoftwareUpdate {
+private:
+    string version;
+    string description;
+    time_t releaseDate;
+public:
+    SoftwareUpdate(string v, string d, time_t r) : version(v), description(d), releaseDate(r) {}
+    void displayUpdateInfo() const {
+        char buffer[26];
+        ctime_s(buffer, sizeof(buffer), &releaseDate);
+        cout << "Версия: " << version << ", Описание: " << description << ", Дата выхода: " << buffer;
+    }
+    bool isAvailable() const {
+        time_t now = time(nullptr);
+        return difftime(now, releaseDate) > 0;
+    }
+};
+
 class Software {
 protected:
     string name;
     string manufacturer;
     string category;
+    vector<SoftwareUpdate> updates;
 public:
     Software(string n, string m, string c) : name(n), manufacturer(m), category(c) {}
     virtual void displayInfo() const = 0;
     virtual bool isAvailable() const = 0;
     virtual ~Software() {}
+    void addUpdate(SoftwareUpdate update) {
+        updates.push_back(update);
+    }
+    void showUpdates() const {
+        cout << "Обновления для " << name << ":\n";
+        for (const auto& update : updates) {
+            update.displayUpdateInfo();
+        }
+    }
 };
 
-// Класс Свободное ПО
 class FreeSoftware : public Software {
 public:
     FreeSoftware(string n, string m, string c) : Software(n, m, c) {}
@@ -53,11 +77,10 @@ public:
     }
 };
 
-// Класс Условно-бесплатное ПО
 class Shareware : public Software {
 private:
     time_t installDate;
-    int trialPeriod; // в днях
+    int trialPeriod;
 public:
     Shareware(string n, string m, string c, time_t d, int t) : Software(n, m, c), installDate(d), trialPeriod(t) {}
     void displayInfo() const override {
@@ -71,12 +94,11 @@ public:
     }
 };
 
-// Класс Коммерческое ПО
 class CommercialSoftware : public Software {
 private:
     double price;
     time_t installDate;
-    int licenseDuration; // в днях
+    int licenseDuration;
 public:
     CommercialSoftware(string n, string m, string c, double p, time_t d, int l) : Software(n, m, c), price(p), installDate(d), licenseDuration(l) {}
     void displayInfo() const override {
@@ -104,20 +126,17 @@ int main() {
     time_t now = time(nullptr);
 
     softwareDatabase.push_back(new FreeSoftware("LibreOffice", "The Document Foundation", "Офисные приложения"));
-    softwareDatabase.push_back(new Shareware("WinRAR", "RARLAB", "Архиваторы и утилиты для сжатия данных", now - 10 * 24 * 60 * 60, 30)); // 10 дней назад, срок 30 дней
-    softwareDatabase.push_back(new CommercialSoftware("Adobe Photoshop", "Adobe", "Графические редакторы", 19999.99, now - 50 * 24 * 60 * 60, 365)); // 50 дней назад, срок 365 дней
+    softwareDatabase.push_back(new Shareware("WinRAR", "RARLAB", "Архиваторы и утилиты для сжатия данных", now - 10 * 24 * 60 * 60, 30));
+    softwareDatabase.push_back(new CommercialSoftware("Adobe Photoshop", "Adobe", "Графические редакторы", 19999.99, now - 50 * 24 * 60 * 60, 365));
+
+    softwareDatabase[2]->addUpdate(SoftwareUpdate("24.1", "Исправления ошибок", now - 5 * 24 * 60 * 60));
+    softwareDatabase[2]->addUpdate(SoftwareUpdate("24.2", "Добавлены новые фильтры", now - 1 * 24 * 60 * 60));
+    softwareDatabase[2]->showUpdates();
 
     cout << "Полная информация о программном обеспечении:\n";
     for (const auto& sw : softwareDatabase) {
         sw->displayInfo();
         cout << "Доступно для использования: " << (sw->isAvailable() ? "Да" : "Нет") << endl << endl;
-    }
-
-    cout << "ПО, доступное для использования:\n";
-    for (const auto& sw : softwareDatabase) {
-        if (sw->isAvailable()) {
-            sw->displayInfo();
-        }
     }
 
     for (auto& sw : softwareDatabase) {
---
+++ threads1.1/threads1.1/threads1.1.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <windows.h>
+#include <ctime>
+
+class NumHandler {
+private:
+    std::string line;
+    std::mutex& mtx;
+
+public:
+    NumHandler(std::mutex& m) : mtx(m), line("123456789") {}
+
+    void add(bool sync) {
+        if (sync) mtx.lock();
+        char digit = '0' + rand() % 10;
+        line += digit;
+        std::cout << "Добавление числа: " << line << std::endl;
+        if (sync) mtx.unlock();
+    }
+
+    void remove(bool sync) {
+        if (sync) mtx.lock();
+        if (!line.empty()) {
+            int pos = rand() % line.size();
+            line.erase(pos, 1);
+            std::cout << "Удаление числа: " << line << std::endl;
+        }
+        if (sync) mtx.unlock();
+    }
+
+    void printStart(const std::string& mode) {
+        std::cout << mode << std::endl;
+        std::cout << "Начальная строка: " << line << std::endl;
+    }
+};
+
+void adder(NumHandler& obj, bool sync) {
+    for (int i = 0; i < 6; ++i) {
+        obj.add(sync);
+        Sleep(100);
+    }
+}
+
+void remover(NumHandler& obj, bool sync) {
+    for (int i = 0; i < 6; ++i) {
+        obj.remove(sync);
+        Sleep(130);
+    }
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    srand(static_cast<unsigned>(time(0)));
+
+    std::mutex mtx;
+
+    NumHandler syncHandler(mtx);
+    syncHandler.printStart("Синхронный режим");
+
+    std::thread t1(adder, std::ref(syncHandler), true);
+    std::thread t2(remover, std::ref(syncHandler), true);
+
+    t1.join();
+    t2.join();
+
+    std::cout << std::endl;
+
+    NumHandler asyncHandler(mtx);
+    asyncHandler.printStart("Асинхронный режим");
+
+    std::thread t3(adder, std::ref(asyncHandler), false);
+    std::thread t4(remover, std::ref(asyncHandler), false);
+
+    t3.join();
+    t4.join();
+
+    std::cout << "\nЗавершено." << std::endl;
+    return 0;
+}
